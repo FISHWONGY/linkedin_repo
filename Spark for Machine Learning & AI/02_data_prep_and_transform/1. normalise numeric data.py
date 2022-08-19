2cd429b41dbcf678c5cd19e3500a33f0c6f5729f
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2022-08-19 22:43:24 +0100

    vc

--- Spark for Machine Learning & AI/02_data_prep_and_transform/1. normalise numeric data.py
+++ Spark for Machine Learning & AI/02_data_prep_and_transform/1. normalise numeric data.py
@@ -21,3 +21,38 @@ sfeatures_df.take(1)
 # Look at the df, now we can see at the new col - sfeatures, the value are all scaled to 0-1
 sfeatures_df.select("features", "sfeatures").show()
 
+
+# Standardise numeric data
+from pyspark.ml.feature import StandardScaler
+from pyspark.ml.linalg import Vectors
+features_df = spark.createDataFrame([
+    (1, Vectors.dense([10.0, 10000.0, 1.0]),),
+    (2, Vectors.dense([20.0, 30000.0, 2.0]),),
+    (3, Vectors.dense([30.0, 40000.0, 3.0]),)
+], ["id", "features"])
+
+# Create standard scaler object
+feature_stand_scaler = StandardScaler(inputCol="features", outputCol="sfeatures", withStd=True, withMean=True)
+# Create model using the scaler object
+stand_smodel = feature_stand_scaler.fit(features_df)
+stand_sfeatures_df = stand_smodel.transform(features_df)
+# check df
+stand_sfeatures_df.take(1)
+# now all features are standardised to -1 - 1
+stand_sfeatures_df.show()
+features_df.show()
+
+
+# Buketise numeric data
+from pyspark.ml.feature import Bucketizer
+splits = [-float("inf"), -10.0, 0.0, 10.0, float("inf")]
+
+b_data = [(-800.0,), (-10.5,), (-1.7,), (0.0,), (8.2,), (90.1,)]
+b_df = spark.createDataFrame(b_data, ["features"])
+b_df.show()
+
+# Create bucketiser obj
+buckertizer = Bucketizer(splits=splits, inputCol="features", outputCol="bfeatures")
+bucketed_df = buckertizer.transform(b_df)
+bucketed_df.show()
+
