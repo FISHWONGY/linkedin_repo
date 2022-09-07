afb7763f38a35e4c7e02380b415b271198cd2d5a
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2022-09-07 23:02:06 +0100

    vc

--- Python_Recursion/05_02/quicksort.py
+++ Python_Recursion/05_02/quicksort.py
@@ -10,7 +10,13 @@ from trace_recursion import trace
 
 
 def quicksort(arr):
-    pass
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
 
 
 def quicksort_verbose(arr):
