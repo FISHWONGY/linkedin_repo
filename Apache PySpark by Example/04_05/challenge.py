ef76e4dfc0c687557a470c8a17434619f48fcf42
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2022-10-09 07:52:39 +0100

    vc

--- Apache PySpark by Example/04_05/challenge.py
+++ Apache PySpark by Example/04_05/challenge.py
@@ -1,11 +1,13 @@
-import pyspark
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 """## Downloading and preprocessing Chicago's Reported Crime Data"""
 
-from pyspark.sql.functions import to_timestamp,col,lit
-rc = spark.read.csv('reported-crimes.csv',header=True).withColumn('Date',to_timestamp(col('Date'),'MM/dd/yyyy hh:mm:ss a')).filter(col('Date') <= lit('2018-11-11'))
+from pyspark.sql.functions import to_timestamp, col, lit
+rc = spark.read.csv('/Users/yuawong/Downloads/reported-crimes.csv',
+                    header=True).withColumn('Date',
+                                            to_timestamp(col('Date'),
+                                                         'MM/dd/yyyy hh:mm:ss a')).filter(col('Date') <= lit('2018-11-11'))
 rc.show(5)
 
 """## Challenge questions
