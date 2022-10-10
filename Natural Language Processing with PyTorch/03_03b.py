b88aa50a878410555dbbd6a5d89d3fac99b3bd23
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2022-10-10 21:26:42 +0100

    vc

--- Natural Language Processing with PyTorch/03_03b.py
+++ Natural Language Processing with PyTorch/03_03b.py
@@ -1,57 +1,57 @@
 import torch.optim as optim
 import torch.nn as nn
 
-criterion = 
-criterion = 
+criterion = nn.CrossEntropyLoss()
+criterion = criterion.to(device)
 
-optimizer =
+optimizer = optim.Adam(model.parameters())
+
 
 def accuracy(predictions, actual_label):
-    max_predictions = predictions.argmax(dim = 1, keepdim = True, )
+    max_predictions = predictions.argmax(dim=1, keepdim=True, )
     correct_predictions = max_predictions.squeeze(1).eq(actual_label)
     accuracy = correct_predictions.sum() / torch.cuda.FloatTensor([actual_label.shape[0]])
     return accuracy
 
-def train(model, iterator, optimizer, criterion):
 
+def train(model, iterator, optimizer, criterion):
     model.train()
     epoch_loss = 0
     epoch_acc = 0
-    
+
     for batch in iterator:
-        # optimizer
-        
-        # predictions
-        
-        # loss
-        
-        # accuracy
-        
+        optimizer.zero_grad()
+
+        predictions = model(batch.text)
+
+        loss = criterion(predictions, batch.label)
+
+        acc = accuracy(predictions, batch.label)
+
         loss.backward()
-        
+
         optimizer.step()
-        
+
         epoch_loss += loss.item()
         epoch_acc += acc.item()
-        
+
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-def evaluate(model, iterator, criterion):
 
+def evaluate(model, iterator, criterion):
     model.eval()
     epoch_loss = 0
     epoch_acc = 0
-    
-    with torch.no_grad():
-    
-        for batch in iterator:
 
+    with torch.no_grad():
+        for batch in iterator:
             predictions = model(batch.text)
-            
+
             loss = criterion(predictions, batch.label)
-            
+
             acc = categorical_accuracy(predictions, batch.label)
             epoch_loss += loss.item()
             epoch_acc += acc.item()
-        
+
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
